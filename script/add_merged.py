87f82bc58d6811d56ffb47976e47c1a78a015faa
oxon <oxon@mac.com>
2020-08-13 22:53:56 +0900

    Add scripts to merge TSV files

---
+++ script/add_merged.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import re
+
+def main(fin1, fin2):
+    # fin1: Aichi_official.tsv or similar
+    # fin2: merged_0807.tsv or similar
+    data1 = []
+    lines = open(fin1).readlines()
+
+    for line in lines:
+        n, date, person, notionality, city, note, another_index = line[:-1].split('\t')
+        data1.append((n, date, person, notionality, city, note, another_index))
+
+    data2= []
+    lines = open(fin2).readlines()[1:]
+    for line in lines:
+        n, date, person, notionality, city, note, another_index = line[:-1].split('\t')
+        data2.append((n, date, person, notionality, city, note, another_index))
+    
+    data12 = data1 + data2
+
+    for line in lines:
+        n, date, person, notionality, city, note, another_index = line[:-1].split('\t')
+        results = re.findall('(愛知県内[,\d]*|豊田市発表[,\d]*|岡崎市発表[,\d]*|豊橋市発表[,\d]*|名古屋市発表[,\d]*|岐阜県内[,\d]*)', note)
+        
+        indices = ''
+        results2 = ''
+
+        for result in results:
+            if result.find('岐阜県内') != 0:
+                note = note.replace(result, '')
+
+            if result.find('愛知県内') == 0:
+                result = result.replace(',', ',愛知県内')
+            elif result.find('豊田市発表') == 0:
+                result = result.replace(',', ',豊田市発表')
+            elif result.find('岡崎市発表') == 0:
+                result = result.replace(',', ',岡崎市発表')
+            elif result.find('豊橋市発表') == 0:
+                result = result.replace(',', ',豊橋市発表')
+            elif result.find('名古屋市発表') == 0:
+                result = result.replace(',', ',名古屋市発表')
+            elif result.find('岐阜県内') == 0:
+                result = result.replace(',', ',岐阜県内')
+
+            results2 += result + ','
+        
+        if len(results2) > 0 and results2[-1] == ',':
+            results2 = results2[:-1]
+            
+        results = results2.split(',')
+
+        for i in range(len(data12) - 1, -1, -1):
+            if data12[i][-1] in results or ('愛知県内' + data12[i][0]) in results:
+                indices += data12[i][0] + ','
+
+        if len(indices) > 0 and indices[-1] == ',':
+            indices = indices[:-1]
+
+        note.replace(' ', '')
+        if note != '':
+            import sys
+            print('WARNING', line, file=sys.stderr, end='')
+            print('*****', note, '*****', file=sys.stderr)
+            note2 = re.sub('(岐阜県内\d*)', '\\1例目', note).replace('例目,岐阜県内', '例目、')
+        elif results == ['']:
+            note2 = ''
+        else:
+            note2 = 'No.' + indices + 'と接触'
+
+        data1.append((n, date, person, notionality, city, note2, another_index))
+
+    for i in range(len(data1)):
+        n, date, person, notionality, city, note, another_index = data1[i]
+        print(n + '\t' + date + '\t' + person + '\t', notionality + '\t' + city + '\t' + note + '\t' + another_index + '\n', end='')
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1], sys.argv[2])
---
+++ script/merge.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+# cd CTV
+# mmdd=0807; ../script/merge.py Aichi_official_$mmdd.tsv Toyota_official_$mmdd.tsv Okazaki_official_$mmdd.tsv Toyohashi_official_$mmdd.tsv Nagoya_official_$mmdd.tsv > merged_$mmdd.tsv
+
+import re
+
+num_Aichi = -1
+
+def tsv(fname):
+    global num_Aichi
+    try:
+        f = open(fname)
+    except:
+        return ''
+
+    text = ''
+
+    for line in f.readlines():
+        if line.find('症例番号') == 0 or line.find('本市公表') == 0:
+            continue # skip header           
+
+        num, age, sex, city, abroad, symp_date, pos_date, level, note = line[:-1].split('\t')
+
+        if fname.find('Aichi') == 0 and num_Aichi == -1:
+            num_Aichi = int(num)
+        else:
+            num_Aichi += 1
+        
+        pub_date = fname.split('_')[-1].split('.')[0]
+        m, d = int(pub_date[:2]), int(pub_date[2:])
+        
+        text += '%d\t' % num_Aichi # No
+        text += '%d月%d日\t' % (m, d) # 発表日
+
+        sex = sex.replace('性', '')
+        if age in ('10未満', '10歳未満'):
+            text += '10歳未満%s性\t' % sex # 年代・性別
+        else:
+            age = int(age)
+            text += '%d代%s性\t' % (age, sex) # 年代・性別
+
+        text += '\t' # 国籍
+        text += city + '\t' # 住居地
+        note = re.sub('（.*?）', '', note) # drop e.g. '（20歳代男性・8月4日）'
+        note = note.replace('例目及び', '例目、')
+        note = note.replace('例目、', ',')
+        note = re.sub('(例目の知人|例目の同僚|例目の家族|例目の濃厚接触者|例目患者の同居家族|例目の友人|例目と接触|例目患者の濃厚接触者|例目患者の同居者)', '', note)
+        note = re.sub('名古屋市発表([,\d]*)。名古屋市発表(\d*)。?', '名古屋市発表\\1,\\2', note)
+        text += note + '\t' # 接触状況
+        if fname.find('Aichi') == 0:
+            text += '本県発表%d\n' % int(num)
+        elif fname.find('Toyota') == 0:
+            text += '豊田市発表%d\n' % int(num)
+        elif fname.find('Okazaki') == 0:
+            text += '岡崎市発表%d\n' % int(num)
+        elif fname.find('Toyohashi') == 0:
+            text += '豊橋市発表%d\n' % int(num)
+        elif fname.find('Nagoya') == 0:
+            text += '名古屋市発表%d\n' % int(num)
+        
+    return text
+
+def main(fnames):
+    print('No\t発表日\t年代・性別\t国籍\t住居地\t接触状況\t備考\n', end='')
+    for fname in fnames:
+        print(tsv(fname), end='')
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1:])
